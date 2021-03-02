716bdec13eaddc4f9dee319c54040d6e1ba53b44
Nathan Reis de Almeida <50592141+Nathan264@users.noreply.github.com>
2021-03-02 14:45:59 -0300

    Addition of header and footer and login style update

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../../public/css/layout.module.css';
 
 function Layout({children}) {
     return (
-        <div className={styles.layout}>
-            {children}
-        </div>
+        <>
+            <nav className={styles.navbar}>
+                 <Link href='/'>Logo</Link> 
+                 <Link href='/login'>Login</Link> 
+            </nav>
+            <main className={styles.layout}>
+                {children}
+            </main>
+
+            <footer className={styles.footer}>
+                <span>By: N</span>
+            </footer>
+        </>
     )
 }
 
