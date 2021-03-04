ba47bad71397dbde8d02ddd9e1406bfa7c007164
Nathan Reis de Almeida <50592141+Nathan264@users.noreply.github.com>
2021-03-04 20:29:37 -0300

    Integration with autocomplete api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import Layout from '../components/layout';
 import '../../public/css/global.css';
 
@@ -6,9 +7,17 @@ import type { AppProps } from 'next/app';
 
 function App({Component, pageProps}: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <script 
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQxI5CKPWrJt1g3Jv32v-m3DYL1G3heX0&libraries=places"
+                    async
+                ></script>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
 
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -17,6 +17,12 @@ function Profile() {
         axios.defaults.headers.common['Authorization'] = `token ${localStorage.getItem('//gitTokenTripee//')}`;
     }
 
+    const attachAutocomplete = () => {
+        const inputLocation = document.getElementById('location');
+        new google.maps.places.Autocomplete(inputLocation);
+    }
+
+
     const loadUserInfo = async () => {
         await axios.get('/user').then(res => {
             setUserInfo({
@@ -40,85 +46,88 @@ function Profile() {
         } catch (error) {
             alert('Houve um erro, verifique as permissões do seu token')
         }
-        
     }
 
+
     useEffect(() => {
-        // loadUserInfo();
+        loadUserInfo();
+        attachAutocomplete();
     }, []);
 
     return (
-        <section className={styles.main}> 
-            <form>
-                <label htmlFor='name'>Name</label> 
-                <input 
-                    type='text' 
-                    name='name' 
-                    value={userInfo.name} 
-                    onChange={e => setUserInfo({...userInfo, name: e.target.value})}
-                    id='name'
-                />
-                
-                <label htmlFor='email'>Email</label> 
-                <input 
-                    type='email' 
-                    name='email' 
-                    value={userInfo.email} 
-                    onChange={e => setUserInfo({...userInfo, email: e.target.value})}
-                    id='email' 
-                />
-                
-                <label htmlFor='location'>Location</label> 
-                <input 
-                    type='text' 
-                    name='location' 
-                    value={userInfo.location} 
-                    onChange={e => setUserInfo({...userInfo, location: e.target.value})}
-                    id='location' 
-                />
-                
-                <label htmlFor='blog'>Blog</label> 
-                <input 
-                    type='text' 
-                    name='blog' 
-                    value={userInfo.blog} 
-                    onChange={e => setUserInfo({...userInfo, blog: e.target.value})}
-                    id='blog' 
-                />
-                
-                <label htmlFor='twitter'>Twitter</label> 
-                <input 
-                    type='text' 
-                    name='twitter' 
-                    value={userInfo.twitter_username} 
-                    onChange={e => setUserInfo({...userInfo, twitter_username: e.target.value})}
-                    id='twitter' 
-                />
-                
-                <label htmlFor='hireable' >Hireable</label>
-                <select 
-                    defaultValue={Number(userInfo.hireable)} 
-                    onChange={e => setUserInfo({...userInfo, hireable: Boolean(e.target.value)})} 
-                    id='hireable'
-                >
-                    <option value={1}>Yes</option>
-                    <option value={0}>No</option>
-                </select>
-                
-                <label htmlFor='bio'>Bio</label> 
-                <textarea 
-                    name='bio' 
-                    value={userInfo.bio} 
-                    onChange={e => setUserInfo({...userInfo, bio: e.target.value})}
-                    id='bio' 
-                />
-                
-                <input 
-                    type='button' 
-                    value='Update' onClick={e => console.log(userInfo)} 
-                />
-            </form>
-        </section>
+        <>
+            <section className={styles.main}> 
+                <form>
+                    <label htmlFor='name'>Name</label> 
+                    <input 
+                        type='text' 
+                        name='name' 
+                        value={userInfo.name} 
+                        onChange={e => setUserInfo({...userInfo, name: e.target.value})}
+                        id='name'
+                    />
+                    
+                    <label htmlFor='email'>Email</label> 
+                    <input 
+                        type='email' 
+                        name='email' 
+                        value={userInfo.email} 
+                        onChange={e => setUserInfo({...userInfo, email: e.target.value})}
+                        id='email' 
+                    />
+                    
+                    <label htmlFor='location'>Location</label> 
+                    <input 
+                        type='text' 
+                        name='location' 
+                        value={userInfo.location} 
+                        onChange={e => setUserInfo({...userInfo, location: e.target.value})}
+                        id='location' 
+                    />
+                    
+                    <label htmlFor='blog'>Blog</label> 
+                    <input 
+                        type='text' 
+                        name='blog' 
+                        value={userInfo.blog} 
+                        onChange={e => setUserInfo({...userInfo, blog: e.target.value})}
+                        id='blog' 
+                    />
+                    
+                    <label htmlFor='twitter'>Twitter</label> 
+                    <input 
+                        type='text' 
+                        name='twitter' 
+                        value={userInfo.twitter_username} 
+                        onChange={e => setUserInfo({...userInfo, twitter_username: e.target.value})}
+                        id='twitter' 
+                    />
+                    
+                    <label htmlFor='hireable' >Hireable</label>
+                    <select 
+                        defaultValue={Number(userInfo.hireable)} 
+                        onChange={e => setUserInfo({...userInfo, hireable: Boolean(e.target.value)})} 
+                        id='hireable'
+                    >
+                        <option value={1}>Yes</option>
+                        <option value={0}>No</option>
+                    </select>
+                    
+                    <label htmlFor='bio'>Bio</label> 
+                    <textarea 
+                        name='bio' 
+                        value={userInfo.bio} 
+                        onChange={e => setUserInfo({...userInfo, bio: e.target.value})}
+                        id='bio' 
+                    />
+                    
+                    <input 
+                        type='button' 
+                        value='Update' onClick={e => sendUserInfo()} 
+                    />
+                </form>
+            </section>
+        </>
     )
 }
 
