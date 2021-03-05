6790065326bbf11f448ebeb1feb442b039bf0f2a
Nathan Reis de Almeida <50592141+Nathan264@users.noreply.github.com>
2021-03-05 19:56:39 -0300

    Implementation of private route rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout';
-import '../../public/css/global.css';
+import axios from '../services/gitApi';
 import config from '../../config.json';
+import '../../public/css/global.css';
 
 import type { AppProps } from 'next/app';
 
 function App({Component, pageProps}: AppProps) {
+    const checkToken = async () => {
+        axios.defaults.headers.common['Authorization'] = `token ${localStorage.getItem('//gitTokenTripee//')}`;
+        try {
+            await axios.get('/user');
+        } catch(error) {
+            alert('Você não tem permissão para acessar essa página');
+            window.location.href = '/'
+        }
+    }
+
+    if(process.browser && window.location.pathname !== '/') {
+        checkToken();
+    }
+
     return (
         <>
             <Head>
