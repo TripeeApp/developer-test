2e6060400ecfcc8d21acec0913324529bdf0e5ba
Nathan Reis de Almeida <50592141+Nathan264@users.noreply.github.com>
2021-03-06 14:38:40 -0300

    Routing Improvements

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout';
 import axios from '../services/gitApi';
@@ -8,31 +8,47 @@ import '../../public/css/global.css';
 import type { AppProps } from 'next/app';
 
 function App({Component, pageProps}: AppProps) {
-    const checkToken = async () => {
-        axios.defaults.headers.common['Authorization'] = `token ${localStorage.getItem('//gitTokenTripee//')}`;
-        try {
-            await axios.get('/user');
-        } catch(error) {
-            alert('Você não tem permissão para acessar essa página');
-            window.location.href = '/'
+    const [canRender, setCanRender] = useState(false);
+
+    useEffect(() => {
+        const checkToken = async () => {
+            axios.defaults.headers.common['Authorization'] = `token ${localStorage.getItem('//gitTokenTripee//')}`;
+            try {
+                await axios.get('/user');
+                setCanRender(true);
+            } catch(error) {
+                alert('Você não tem permissão para acessar essa página');
+                window.location.href = '/'
+                setCanRender(false)
+            }
         }
-    }
-
-    if(process.browser && window.location.pathname !== '/') {
-        checkToken();
-    }
-
+        
+        if(process.browser && window.location.pathname !== '/') {
+            checkToken();
+        } else {
+            setCanRender(true)
+        }
+    }, []);    
+    
     return (
         <>
-            <Head>
-                <script 
-                    src={`https://maps.googleapis.com/maps/api/js?key=${config.googleApiKey}&libraries=places`}
-                    async
-                ></script>
-            </Head>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            {
+                !canRender ? (
+                    <></>
+                ) : (
+                    <>
+                        <Head>
+                            <script 
+                                src={`https://maps.googleapis.com/maps/api/js?key=${config.googleApiKey}&libraries=places`}
+                                async
+                            ></script>
+                        </Head>
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    </>
+                )
+            }
         </>
     )
 }
